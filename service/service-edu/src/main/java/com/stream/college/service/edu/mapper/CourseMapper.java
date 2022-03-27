a6c731ba5d35ecff677171f0289ebc4d4ed43f14
stream1080 <fmxhax638@foxmail.com>
2022-03-27 13:32:03 +0800

    feat: 完成课程管理相关 mapper

--- service/service-edu/src/main/java/com/stream/college/service/edu/mapper/CourseMapper.java
+++ service/service-edu/src/main/java/com/stream/college/service/edu/mapper/CourseMapper.java
@@ -1,7 +1,15 @@
 package com.stream.college.service.edu.mapper;
 
-import com.stream.college.service.edu.entity.Course;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.stream.college.service.edu.entity.Course;
+import com.stream.college.service.edu.entity.vo.CoursePublishVo;
+import com.stream.college.service.edu.entity.vo.CourseVo;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +20,11 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2022-02-02
  */
 public interface CourseMapper extends BaseMapper<Course> {
+
+    List<CourseVo> selectPageByCourseQueryVo(
+            Page<CourseVo> pageParam,
+            @Param(Constants.WRAPPER) QueryWrapper<CourseVo> queryWrapper);
+
+    CoursePublishVo selectCoursePublishVoById(String id);
 
 }
